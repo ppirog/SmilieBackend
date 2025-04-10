648c8ae69d88e8a592d090a94f8be7cf07c8f65e
pawel <pawelpirog99@gmail.com>
2025-04-10 18:47:37 +0200

    deleted unused import

--- src/main/java/com/smilie/smiliebackend/joke/service/JokeService.java
+++ src/main/java/com/smilie/smiliebackend/joke/service/JokeService.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Service;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
 
 @Log4j2
 @Service
